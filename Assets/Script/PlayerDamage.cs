85df66c66b78d2c384a5171eefcc4f1ba6f54754
takeru-tochigi <107602501+takeru-tochigi@users.noreply.github.com>
2023-01-13 20:39:39 +0900

    修正
    障害物の実装の修正をしました。

--- Assets/Script/PlayerDamage.cs
+++ Assets/Script/PlayerDamage.cs
@@ -6,39 +6,42 @@ public class PlayerDamage : MonoBehaviour
 {
     public int PlayerHP;//体力の実装時に置き換え
     public int Mileage;//走行距離
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine("LossHealth");
+      
     }
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "ObstacleStone")//綴りミスきおつけて
         {
-            //Destroy(other.gameObject);
-            //int a;
-            // double x = Time.deltaTime;//変換式
-       //IEnumerator LossHealth()
-         //{
-            // PlayerHP = PlayerHP - a;
-            // yield return new WaitForSeconds(10.0f);
-            // PlayerHP = PlayerHP - 0;１０秒間体力を減らす
-            //頭上に画像出現
-          //}
-            
-        }
-
-        if (other.gameObject.tag == "ObstacleRottenOnigiri")//綴りミスきおつけて
-        {
             Destroy(other.gameObject);
-            //10秒間スピードの変数を０にする＆３０秒間おにぎりのタグを無効化したい（物体自体を向こうでもいい）
+            count += 1;
+            if(count <= 10)
+            {
+                Status.HealthGauge -= 1; 
+            }
         }
-
+        //if (other.gameObject.tag == "ObstacleRottenOnigiri")//綴りミスきおつけて
+        //{
+            //Destroy(other.gameObject);
+           // count += 1;
+            //if(count <= 10)
+           //{
+                //Speed = 0;
+           // }
+            //if(count <= 30)
+           // {
+               // Destroy(Onigiri.tag);
+           //}
+            //10秒間スピードの変数を０にする＆３０秒間おにぎりのタグを無効化したい（物体自体を向こうでもいい）
+       // }
         if (other.gameObject.tag == "ObstacleRunner")//綴りミスきおつけて
         {
             Destroy(other.gameObject);
-            Mileage = Mileage - 1000;//走行距離の変数
+            Distance.distance = Distance.distance - 1000;//走行距離の変数
         }
     }
 }
