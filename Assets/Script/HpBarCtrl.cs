fdb8377a674cd30cd815cb645490ab7fc0d2b223
takeru-tochigi <107602501+takeru-tochigi@users.noreply.github.com>
2023-01-21 02:15:04 +0900

    修正と追加
    ステータスの修正とゲージの追加

---
+++ Assets/Script/HpBarCtrl.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class HpBarCtrl : MonoBehaviour
+{
+    Slider _slider1;
+    Slider _slider2;
+    Slider _slider3;
+    Slider _slider4;
+    void Start()
+    {
+        // スライダーを取得する
+        _slider1 = GameObject.Find("SliderWaterGauge").GetComponent<Slider>();
+        _slider2 = GameObject.Find("SliderSatietyGauge").GetComponent<Slider>();
+        _slider3 = GameObject.Find("SliderStaminaGauge").GetComponent<Slider>();
+        _slider4 = GameObject.Find("SliderHealthGauge").GetComponent<Slider>();
+
+    }
+    float _hp1 = 0;
+    float _hp2 = 0;
+    float _hp3 = 0;
+    float _hp4 = 0;
+
+    void Update()
+    {
+        _hp1 = Status.WaterGauge;
+        _hp2 = Status.SatietyGauge;
+        _hp3 = Status.StaminaGauge;
+        _hp4 = Status.HealthGauge;
+
+        _slider1.value = _hp1;
+        _slider2.value = _hp2;
+        _slider3.value = _hp3;
+        _slider4.value = _hp4;
+    }
+}
