920bf68fc48bb02640333e8d6267fcb146d802fd
takeru-tochigi <107602501+takeru-tochigi@users.noreply.github.com>
2023-02-20 20:58:22 +0900

    調整
    ステータスゲージの調整

--- Assets/Script/Status.cs
+++ Assets/Script/Status.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Status : MonoBehaviour
 {
-    public static int SatietyGauge;//満腹ゲージ90秒で0%になるようにしてください
-    public static int WaterGauge;//水分ゲージ90秒で0%になるようにしてください
-    public static int StaminaGauge;//スタミナゲージ120秒で0になるようにしてください。満腹度と水分が50%以上のときにスタミナが回復する。回復量は40秒で0から100に回復できる程度の速度
-    public static int HealthGauge;//体力ゲージ水分ゲージが0のとき,スタミナゲージが0のときそれぞれ毎秒2ずつ減らしてください。重複可能
+    public static float SatietyGauge;//満腹ゲージ90秒で0%になるようにしてください
+    public static float WaterGauge;//水分ゲージ90秒で0%になるようにしてください
+    public static float StaminaGauge;//スタミナゲージ120秒で0になるようにしてください。満腹度と水分が50%以上のときにスタミナが回復する。回復量は40秒で0から100に回復できる程度の速度
+    public static float HealthGauge;//体力ゲージ水分ゲージが0のとき,スタミナゲージが0のときそれぞれ毎秒2ずつ減らしてください。重複可能
     private float currentTime = 0f;
 
     void Start()
     {
         SatietyGauge = 100;
         WaterGauge = 100;
-        StaminaGauge = 120;
+        StaminaGauge = 100;
         HealthGauge = 100;
     }
     void Update()
@@ -29,13 +29,13 @@ public class Status : MonoBehaviour
         {
             SatietyGauge -= 100 / 90;
             WaterGauge -= 100 / 90;
-            StaminaGauge -= 120 / 120;
+            StaminaGauge -= 100 / 120;
             HealthGauge -= 0;
             //水分、満腹ゲージを90秒で0%になるようにしてください
             //スタミナゲージ120秒で0になるようにしてください
-            if (SatietyGauge >= 120 / 2 && WaterGauge >= 100 / 2)
+            if (SatietyGauge >= 100 / 2 && WaterGauge >= 100 / 2)
             {
-                StaminaGauge = StaminaGauge + 120 / 40;
+                StaminaGauge = StaminaGauge + 100 / 40;
             }
             //満腹度と水分が50%以上のときにスタミナが回復する。
             //回復量は40秒で0から100に回復できる程度の速度
